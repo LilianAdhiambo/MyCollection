d8556eeb61efca33d9d38faf40f540878ddfa9df
LilianAdhiambo <lizmadhiambo@gmail.comgit config --listgit config --global user.email lizmadhiambo@gmail.com>
2014-10-28 12:56:09 +0300

    Initial commit

---
+++ src/com/example/MyCollection/HomeActivity.java
@@ -0,0 +1,1 @@
+package com.example.MyCollection;import android.app.Activity;import android.os.Bundle;public class HomeActivity extends Activity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.home);    }}
---
+++ src/com/example/MyCollection/MyActivity.java
@@ -0,0 +1,1 @@
+package com.example.MyCollection;import android.app.Activity;import android.os.Bundle;public class MyActivity extends Activity {    /**     * Called when the activity is first created.     */    @Override    public void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.main);    }}
